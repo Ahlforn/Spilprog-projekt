90d14589645296fa45435ec54ad26acd20e46f6d
Simone Jørgensen <Simoneljoergensen@gmail.com>
2019-04-11 11:51:30 +0200

    Scripts til kamera og actor

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    //speed skal sætte så det er det samme som actors speed ellers følges de ikke ad.
+    public float cameraSpeed = 4f;
+    
+   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        pos.z += cameraSpeed * Time.deltaTime;
+
+        transform.position = pos;
+
+        
+        
+
+        
+
+    }
+}
